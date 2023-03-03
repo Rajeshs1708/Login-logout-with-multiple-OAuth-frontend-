97e1d94bd0ef03e4fd337292ec36906451ea9796
Rajesh <rajeshs1097@gmail.com>
2023-03-04 01:02:45 +0530

    first commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,24 @@
 import React,{useState} from 'react';
 import './Card.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Link } from 'react-router-dom';
 import { Heart } from "react-bootstrap-icons";
 
 
 const Card = ({ post }) => {
     const [color, setColor] = useState(false)
+
+    const handleChange=()=>{
+        setColor(color=>!color)
+        if(color){
+            const notify = () => toast.success("Added to favourite", { autoClose: 3000, theme: "colored", });
+            notify()
+        }else{
+            const notify = () => toast.success("Removed to favourite", { autoClose: 3000, theme: "colored", });
+            notify()
+        }
+    }
 
     return (
         <div className='card col-sm-12 col-md-6 col-lg-4'>
@@ -14,7 +27,7 @@ const Card = ({ post }) => {
             <p style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>{post.desc}</p>
             <div className='favourite'>
                 <Link to={`/post/${post.id}`} className='link' ><button className='cardButton'>Read More</button></Link>
-                <Heart style={{backgroundColor:`${color?"#E75480":""}`}} size={20} onClick={()=>{setColor(color=>!color)}} />
+                <Heart style={{backgroundColor:`${color?"#E75480":""}`}} size={20} onClick={handleChange} />
             </div>
         </div>
 
