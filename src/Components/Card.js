f8046fed88e84917ee815df9fbe82403645a4645
Rajesh <rajeshs1097@gmail.com>
2023-03-04 01:46:36 +0530

    first commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,38 +1,60 @@
-import React,{useState} from 'react';
-import './Card.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Link } from 'react-router-dom';
-import { HeartFill } from "react-bootstrap-icons";
-
+import React, { useState } from 'react'
+import './Card.css'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Link } from 'react-router-dom'
+import { HeartFill } from 'react-bootstrap-icons'
 
 const Card = ({ post }) => {
-    const [color, setColor] = useState(false)
+  const [color, setColor] = useState(false)
 
-    const handleChange=()=>{
-        setColor(color=>!color)
-        if(!color){
-            const notify = () => toast.success("Added to favourite", {position: "top-center", autoClose: 3000, theme: "colored", });
-            notify()
-        }else{
-            const notify = () => toast.error("Removed to favourite", {position: "top-center", autoClose: 3000, theme: "colored", });
-            notify()
-        }
+  const handleChange = () => {
+    setColor(color => !color)
+    if (!color) {
+      const notify = () =>
+        toast.success('Added to favourite', {
+          position: 'top-center',
+          autoClose: 3000,
+          theme: 'colored'
+        })
+      notify()
+    } else {
+      const notify = () =>
+        toast.error('Removed to favourite', {
+          position: 'top-center',
+          autoClose: 3000,
+          theme: 'colored'
+        })
+      notify()
     }
+  }
 
-    return (
-        <div className='card text-center col-sm-12 col-md-6 col-lg-4'>
-            <span className='title h4'>{post.title}</span>
-            <img className='img img-fluid mx-auto' alt={post.alt} src={post.img} />
-            <p style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>{post.desc}</p>
-            <div className='favourite'>
-                <Link to={`/post/${post.id}`} className='link' ><button className='cardButton'>Read More</button></Link>
-                <HeartFill style={{color:`${color?"#E75480":""}`,cursor:"pointer"}} size={20} onClick={handleChange} />
-                <ToastContainer autoClose={3000} theme="colored" position="top-center" />
-            </div>
-        </div>
-
-    )
+  return (
+    <div className='card text-center col-sm-12 col-md-6 col-lg-4'>
+      <span className='title h4'>{post.title}</span>
+      <img className='img img-fluid mx-auto' alt={post.alt} src={post.img} />
+      <p
+        style={{
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis'
+        }}
+      >
+        {post.desc}
+      </p>
+      <div className='favourite'>
+        <Link to={`/post/${post.id}`} className='link'>
+          <button className='cardButton'>Read More</button>
+        </Link>
+        <HeartFill
+          style={{ color: `${color ? '#E75480' : ''}`, cursor: 'pointer' }}
+          size={20}
+          onClick={handleChange}
+        />
+      </div>
+      <ToastContainer autoClose={3000} theme='colored' position='top-center' />
+    </div>
+  )
 }
 
 export default Card
