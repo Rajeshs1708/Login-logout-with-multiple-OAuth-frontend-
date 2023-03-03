739499f2d4a77e35e40db53bd8cf4801e9b76f49
Rajesh <rajeshs1097@gmail.com>
2023-03-04 00:55:24 +0530

    first commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,20 +1,20 @@
 import React,{useState} from 'react';
 import './Card.css';
 import { Link } from 'react-router-dom';
-import { SearchHeart } from "react-bootstrap-icons";
+import { Heart } from "react-bootstrap-icons";
 
 
 const Card = ({ post }) => {
     const [color, setColor] = useState(false)
 
     return (
-        <div className='card col-sm-12 col-md-6 col-lg-4  text-center'>
-            <span className='title h4'>{post.title}</span>
+        <div className='card col-sm-12 col-md-6 col-lg-4'>
+            <span className='title   text-center h4'>{post.title}</span>
             <img className='img' alt={post.alt} src={post.img} />
             <p style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>{post.desc}</p>
             <div className='favourite'>
                 <Link to={`/post/${post.id}`} className='link' ><button className='cardButton'>Read More</button></Link>
-                <SearchHeart style={{color:`${color?"#E75480":""}`}} onClick={()=>{setColor(color=>!color)}} />
+                <Heart style={{color:`${color?"#E75480":""}`}} size={20} onClick={()=>{setColor(color=>!color)}} />
             </div>
         </div>
 
