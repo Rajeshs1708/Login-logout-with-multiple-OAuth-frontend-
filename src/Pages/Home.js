147e7525b0661bf871fa223de49e681b9d86f5a0
Rajesh <rajeshs1097@gmail.com>
2023-03-04 00:24:33 +0530

    first commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,75 +1,82 @@
-import React,{useEffect} from 'react';
-import './Home.css';
-import axios from 'axios';
-import { posts } from '../data';
-import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Card from '../Components/Card';
-import Charts from '../Components/Charts';
+import React, { useEffect } from 'react'
+import './Home.css'
+import axios from 'axios'
+import { posts } from '../data'
+import { useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Card from '../Components/Card'
+import Charts from '../Components/Charts'
 
 const Home = ({ user }) => {
-
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
     const oauthUser = user
-    const token = localStorage.getItem('TOKEN');
+    const token = localStorage.getItem('TOKEN')
     if (!token && !oauthUser) {
       navigate('/login')
     } else {
       navigate('/')
     }
-  }, [navigate,user])
+  }, [navigate, user])
 
   const handleLogout = () => {
     try {
-      axios.get(`${process.env.REACT_APP_BASE_URL}/api/signout`)
+      axios
+        .get(`${process.env.REACT_APP_BASE_URL}/api/signout`)
         .then(res => {
           if (res) {
-            const notify = () => toast.success(`*${res.data.message}*`, { theme: 'colored' });
+            const notify = () =>
+              toast.success(`*${res.data.message}*`, { theme: 'colored' })
             notify()
-            localStorage.removeItem("TOKEN");
-            localStorage.removeItem("NAME");
-            localStorage.removeItem("EMAIL");
+            localStorage.removeItem('TOKEN')
+            localStorage.removeItem('NAME')
+            localStorage.removeItem('EMAIL')
             setTimeout(() => {
               navigate('/login')
             }, 1000)
-
           }
         })
         .catch(err => {
-          const notify = () => toast.error(`*${err.response.data.message}*`, { theme: 'colored' });
+          const notify = () =>
+            toast.error(`*${err.response.data.message}*`, { theme: 'colored' })
           notify()
         })
-
-
     } catch (err) {
-      console.log("Error...", err);
+      console.log('Error...', err)
     }
   }
   return (
     <>
-      <div className=''>
-        <p className='h5 text-center'>Welcome : {localStorage.getItem('NAME')}</p>
-        <button className='btn pe-4' style={{backgroundColor:"#ADD8E6"}} title="This is for input logout" onClick={handleLogout}>Logout</button>
-        <ToastContainer autoClose={3000} theme="colored" />
-      </div>
+      <div className='container-fluid'>
+        <div className='row'>
+          <p className='h5 text-center'>
+            Welcome : {localStorage.getItem('NAME')}
+          </p>
+          <button
+            className='btn pe-4'
+            style={{ backgroundColor: '#ADD8E6' }}
+            title='This is for input logout'
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+          <ToastContainer autoClose={3000} theme='colored' />
+        </div>
 
-      <div className='text-center'>
-        <h4 className='text-center display-6'>Most popular wonders of the world</h4>
-        <Charts />
-      </div>
+        <div className='row text-center'>
+          <h4>Most popular wonders of the world</h4>
+          <Charts />
+        </div>
 
-      <div className='home'>
-        <p className='text-center display-6 topic'>7 Wonders of the world</p>
-        {
-          posts.map(post => (
+        <div className='row home'>
+          <p className='text-center display-6 topic'>7 Wonders of the world</p>
+          {posts.map(post => (
             <Card key={post.id} post={post} />
-          ))
-        }
+          ))}
+        </div>
       </div>
-
     </>
   )
 }
