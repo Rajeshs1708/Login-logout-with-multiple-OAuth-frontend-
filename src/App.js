d04f4600865cda621ae111dc8c20f5d79357adc0
Rajesh <rajeshs1097@gmail.com>
2023-03-08 16:26:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import axios from "axios";
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Post from './Pages/Post';
-import Navbar from './Components/Navbar';
-import ForgetPassword from './Pages/ForgetPassword';
-import NewPassword from './Pages/NewPassword';
-import Signup from './Pages/Signup';
-import Charts from './Components/Charts';
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Post from './Pages/Post'
+import Navbar from './Components/Navbar'
+import ForgetPassword from './Pages/ForgetPassword'
+import NewPassword from './Pages/NewPassword'
+import Signup from './Pages/Signup'
+import Charts from './Components/Charts'
 
-function App() {
-
-  const [user, setUser] = useState(null);
+function App () {
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     const getUser = async () => {
-      try {
-        // const url = `${process.env.REACT_APP_BASE_URL}/auth/login/success`
-        const url = `${process.env.REACT_APP_BASE_URL}/api/getUser`
-        const { data } = await axios.get(url, { withCredentials: true })
-        setUser(data.user)
-      } catch (error) {
-        console.log(`Error While login : ${error}`);
+      const userDetails = {
+        name: localStorage.getItem('NAME'),
+        token: localStorage.getItem('TOKEN')
       }
+      setUser(userDetails)
+      // try {
+      //   const url = `${process.env.REACT_APP_BASE_URL}/auth/login/success`
+      //   const { data } = await axios.get(url, { withCredentials: true })
+      //   setUser(data.user)
+      // } catch (error) {
+      //   console.log(`Error While login : ${error}`);
+      // }
     }
-    getUser();
-  }, []);
+    getUser()
+  }, [])
 
-console.log(user);
-  
-return (
-    <Router >
-      <div className="app">
+  console.log(user)
+
+  return (
+    <Router>
+      <div className='app'>
         <Navbar user={user} />
         <Routes>
           <Route path='/' element={<Home user={user} />} />
-          <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
+          <Route
+            path='/login'
+            element={user ? <Navigate to='/' /> : <Login />}
+          />
           <Route path='/signup' element={<Signup />} />
           <Route path='/forgetpassword' element={<ForgetPassword />} />
           <Route path='/newPassword' element={<NewPassword />} />
@@ -46,7 +57,7 @@ return (
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
