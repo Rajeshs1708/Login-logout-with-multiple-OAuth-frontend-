d235f8b09063736ea32c8b309310b5cef96ceacb
Rajesh <rajeshs1097@gmail.com>
2023-03-08 13:44:33 +0530

    first commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom'
 import { BoxArrowLeft, Person, SearchHeart } from 'react-bootstrap-icons'
 
 function Navbar ({ user }) {
+  let user2 = {
+    name: localStorage.getItem('NAME'),
+    email: localStorage.getItem('NAME'),
+    token: localStorage.getItem('NAME')
+  }
   const logout = () => {
     window.open(`${process.env.REACT_APP_BASE_URL}/auth/logout`, '_self')
   }
@@ -13,14 +18,18 @@ function Navbar ({ user }) {
       <span className='logo'>
         Find Articles <SearchHeart className='searchIcon' />
       </span>
-      {user ? (
+      {user || user2 ? (
         <ul className='list d-flex align-items-center list-unstyled'>
-          <li className='listItem text-center pe-4'><Person />{user.name}</li>
+          {/* <li className='listItem text-center pe-4'>
+            <Person />
+            {user.name}
+          </li> */}
           <li
             className='listItem text-center pe-4'
             title='This is for OAuth'
             onClick={logout}
-          ><BoxArrowLeft />
+          >
+            <BoxArrowLeft />
             Logout
           </li>
         </ul>
